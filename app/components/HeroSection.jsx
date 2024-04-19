f581d281923b6eccb6ae0d40533c35187c3ae687
jairahfigueroa <jairah_figueroa323@yahoo.com>
2024-04-19 13:56:16 -0700

    Newly added

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -24,10 +24,6 @@ const HeroSection = () => {
               sequence={[
                 "Jairah Figueroa",
                 1000,
-                "Jairah Figueroa",
-                1000,
-                "Jairah Figueroa",
-                1000,
               ]}
               wrapper="span"
               speed={50}
